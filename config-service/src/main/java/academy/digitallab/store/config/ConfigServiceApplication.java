ce6d6c4663f35abcd845a1536d2d140c94758939
MauroRussi <mauricio.russi@fair.com>
2022-07-26 13:47:31 +0200

    Disabled CSRF

--- config-service/src/main/java/academy/digitallab/store/config/ConfigServiceApplication.java
+++ config-service/src/main/java/academy/digitallab/store/config/ConfigServiceApplication.java
@@ -3,9 +3,11 @@ package academy.digitallab.store.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableWebSecurity
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
