1bf987f2a436d246798ca1b1eeaab04f4764de1d
MauroRussi <mauricio.russi@fair.com>
2022-08-01 18:52:20 +0200

    Add admin-service project.

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if gradle is installed
+if ! gradle -version > /dev/null 2>&1; then
+  echo "This script uses gradle and it isn't installed - please install it and try again!"
+  exit 1
+fi
+
+# Clean gradle previous configuration
+rm -rf .gradle gradle gradlew* build
+
+# Build all modules using gradle
+gradle build
